cb31238c2875def07b6e044c19005d4394f9f35e
Antonino Francesco Musolino <ninomusolino@gmail.com>
2016-11-10 09:52:28 +0100

    Documentation
    Added some documentation

--- Classes/UIView+DragDrop.h
+++ Classes/UIView+DragDrop.h
@@ -23,25 +23,33 @@ typedef NS_ENUM( NSInteger, UIViewDragDropMode) {
 /**
  *  Set up drag+drop
  *  @params
- *    views: NSArray of drop views
+ *    dropViews: NSArray of drop views
  *    delegate: id delegate conforming to UIViewDragDropDelegave protocol
  */
 - (void) enableDraggingWithDropViews:(NSArray*)dropViews delegate:(id<UIViewDragDropDelegate>)delegate;
 
+/// Set up drag+drop without drop views and delegate
 - (void) enableDragging;
 
+/// Stop dragging on the current view
 - (void) stopDragging;
 
+/// Set up the delegate for the UIViewDragDropDelegate Protocol
 - (void) setDragDropDelegate:(id<UIViewDragDropDelegate>)delegate;
 
+/// Set up o change the Drag mode
 - (void) changeDragMode:(UIViewDragDropMode)mode;
 
+/// Set up o change the Drop Views
 - (void) changeDropViews:(NSArray*)views;
 
+/// Set up o change the returning to the initial position animation duration
 - (void) changeAnimationDuration:(float)seconds;
 
+/// Save the initial position of the view
 - (void) saveStartingPosition:(BOOL)flag;
 
+/// Start the returning to the initial position animation
 - (void) moveToStartingPosition;
 
 @end
@@ -51,14 +59,27 @@ typedef NS_ENUM( NSInteger, UIViewDragDropMode) {
  */
 @protocol UIViewDragDropDelegate <NSObject>
 
+/**
+ * This method informs you that your view was dropped on one of your drop views
+ * @params
+ *      view: your draggable view
+ *      drop: the drop view on which your draggable view was dropped
+ */
 - (void) view:(UIView *)view wasDroppedOnDropView:(UIView *)drop;
 
 @optional
 
+/**
+ * This method informs you that your view is about to return to its initial position
+ * @params
+ *      view: your draggable view
+ */
 - (BOOL) viewShouldReturnToStartingPosition:(UIView*)view;
 
+/// This method informs you that your view returned to its initial position
 - (void) viewDidReturnToStartingPosition;
 
+/// This method informs you that your view returned to its initial position
 - (void) draggingDidBeginForView:(UIView*)view;
 
 - (void) draggingDidEndOutsideDropView:(UIView*)view;
